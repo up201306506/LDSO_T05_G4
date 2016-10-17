ef6a8c36899510df2b8f9741873908aad1c059c7
PedroJoaoPereira <pedro.pereira.95@hotmail.com>
2016-10-17 20:24:05 +0100

    Semi BD

--- Code/app.js
+++ Code/app.js
@@ -5,58 +5,31 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-// web pages js's
-var routes = require('./routes/index');
-var test = require('./routes/test');
-
+// web app
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+// settings
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// web pages js's
+var index = require('./routes/index');
+
 // declaration of the web pages
-app.use('/', routes);
-app.use('/test', test);
+app.use('/', index);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
+// catch 404
+app.use(function (req, res, next) {
     res.render('error', {
-      message: err.message,
-      error: err
+        error: '404 Page Not Found'
     });
-  });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
 });
-
 
 module.exports = app;
--- Code/routes/index.js
+++ Code/routes/index.js
@@ -1,9 +1,52 @@
 var express = require('express');
 var router = express.Router();
+var mongo = require('mongodb').MongoClient;
+var assert = require('assert');
+
+var url = 'mongodb://localhost:27017/test';
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Local Exchange' });
+router.get('/', function (req, res, next) {
+    res.render('index', {title: 'Local Exchange'});
+});
+
+router.post('/register', function (req, res, next) {
+
+    var itemToRegister = {
+        username: req.body.username,
+        password: req.body.password
+    };
+
+    mongo.connect(url, function (err, db) {
+        assert.equal(null, err);
+        db.collection('users').insertOne(itemToRegister, function (err, result) {
+            assert.equal(null, err);
+            db.close();
+        })
+    });
+
+    res.redirect('/');
+});
+
+router.post('/login', function (req, res, next) {
+
+    var usernameReceived = req.body.name;
+
+    var resultArray = [];
+    mongo.connect(url, function (err, db) {
+        assert.equal(null, err);
+        var cursor = db.collection('users').find();
+        cursor.forEach(function (doc, err) {
+            assert.equal(null, err);
+            resultArray.push(doc);
+        }, function () {
+            db.close();
+        });
+    });
+
+    console.log(resultArray);
+
+    res.redirect('/');
 });
 
 module.exports = router;
