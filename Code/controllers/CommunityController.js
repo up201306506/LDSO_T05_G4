bdc2c1a2ac88ea0b63658e25e3d1ea272beab681
up201305617 <up201305617@fe.up.pt>
2016-11-13 14:47:47 +0000

    Fundar comunidade - done

--- Code/controllers/CommunityController.js
+++ Code/controllers/CommunityController.js
@@ -2,25 +2,28 @@ var MongoClient = require('mongodb').MongoClient, assert = require('assert');
 var configDB = require('./../config/database.js');
 var url = configDB.url;
 
-var insertCommunity = function (db, name, headOffice, category, founder, description, privacy, coin, creationDate,members,callback){
-    var community = db.collection('community');
-    community.find({name:name, date: creationDate }).toArray(function (err,docs) {
-        assert.equal(err,null);
-        if(docs.length>=1){
-            console.log('Already exists a community with the given name and creation date');
-            db.close();
-        }
-        else{
-            community.insertOne({name: name, office: headOffice, category: category, description: description, date: creationDate, founder: founder, privacy: privacy, has_coin: coin, members: members},
-                function (err,result) {
-                    assert.equal(err,null);
-                    assert.equal(1, result.result.n);
-                    assert.equal(1, result.ops.length);
-                    console.log('Inserted 1 document into the db');
-                    callback(result);
-                });
-        }
-    });
+module.exports = {
+
+    insertCommunity : function (db, name, headOffice, category, founder, description, privacy, creationDate,members,callback){
+        var community = db.collection('community');
+        community.find({name:name, date: creationDate }).toArray(function (err,docs) {
+            assert.equal(err,null);
+            if(docs.length>=1){
+                console.log('Already exists a community with the given name and creation date');
+                db.close();
+            }
+            else{
+                community.insertOne({name: name, office: headOffice, category: category, description: description, date: creationDate, founder: founder, privacy: privacy, members: members},
+                    function (err,result) {
+                        assert.equal(err,null);
+                        assert.equal(1, result.result.n);
+                        assert.equal(1, result.ops.length);
+                        console.log('Inserted 1 document into the db');
+                        callback(result);
+                    });
+            }
+        });
+    }
 }
 
 var listAllCommunities = function (db,callback) {
--- Code/routes/create_community.js
+++ Code/routes/create_community.js
@@ -1,11 +1,66 @@
 var express = require('express');
 var router = express.Router();
 var assert = require('assert');
+var configDB = require('./../config/database.js');
+var communityController = require('../controllers/CommunityController');
 
 /* GET community creation page. */
 router.get('/', function (req, res) {
     res.render('community/create_community', {title: 'New Community'});
 });
 
+var getDateTime = function () {
+
+    var date = new Date();
+
+    var hour = date.getHours();
+    hour = (hour < 10 ? "0" : "") + hour;
+
+    var min  = date.getMinutes();
+    min = (min < 10 ? "0" : "") + min;
+
+    var sec  = date.getSeconds();
+    sec = (sec < 10 ? "0" : "") + sec;
+
+    var year = date.getFullYear();
+
+    var month = date.getMonth() + 1;
+    month = (month < 10 ? "0" : "") + month;
+
+    var day  = date.getDate();
+    day = (day < 10 ? "0" : "") + day;
+
+    return year + ":" + month + ":" + day + ":" + hour + ":" + min + ":" + sec;
+}
+
+router.post('/create', function (req,res)
+{
+    var user_data;
+
+    if(req.isAuthenticated())
+        user_data = req.user;
+
+    req.checkBody('nome', 'Nome da comunidade necessário').notEmpty();
+    req.checkBody('sede', 'Sede da comunidade necessária').notEmpty();
+    req.checkBody('descricao', 'Descricao da comunidade necessária').notEmpty();
+
+    var errors = req.validationErrors();
+    if (errors) {
+        console.log("Erros SIM");
+        res.render('community/create_community', {
+            title: 'New Community',
+            errors: errors
+        });
+    } else {
+        mongo.connect(configDB.url, function (err, db, next) {
+            communityController.insertCommunity(db, req.body.nome, req.body.sede, req.body.categoria, user_data, req.body.descricao, req.body.visualizacao, getDateTime(),[user_data], function () {
+                db.close();
+            });
+        });
+
+        req.flash('success_msg', 'Comunidade criada com sucesso');
+        res.redirect('/');
+    }
+})
 
 module.exports = router;
