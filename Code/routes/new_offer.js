65d9217ed367d422ab70ed37f38d4b8738ad986d
telmo08 <telmo_8b@hotmail.com>
2016-11-14 15:53:10 +0000

    Create offer

--- Code/routes/new_offer.js
+++ Code/routes/new_offer.js
@@ -1,22 +1,27 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express'),
+    router = express.Router(),
+    mongo = require('mongodb').MongoClient,
+    assert = require('assert'),
+    configDB = require('./../config/database.js'),
+    userPrivileges = require('./../config/userPrivileges'),
+    offerController = require('../controllers/OfferController');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', userPrivileges.ensureAuthenticated, function(req, res, next) {
     res.render('new_offer', { title: 'New Offer' });
 });
 
-router.post('/', function(req, res) {
+router.post('/', function(req, res, next) {
+    req.checkBody('offer_name', 'Nome da oferta é necessário').notEmpty();
+    req.checkBody('offer_description', 'Username é necessário').notEmpty();
+    req.checkBody('offer_price', 'Username é necessário').notEmpty();
 
-    var response =
-    {
-        offer_name:req.body.offer_name,
-        offer_description:req.body.offer_description,
-        offer_type:req.body.offer_type
-    };
-
-    console.log(response);
-    res.end(JSON.stringify(response));
+    mongo.connect(configDB.url, function (err, db, next) {
+        offerController.insertOffer(db, req.body.optradio, req.user, req.body.offer_description, false, Date.now(), req.body.offer_price, req.body.offer_name, req.body.offer_image,  function (result, id) {
+            db.close();
+            res.redirect('/viewoffer?id=' + id);
+        });
+    });
 });
 
 module.exports = router;
