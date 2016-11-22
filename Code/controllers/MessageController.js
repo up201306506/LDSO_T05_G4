74a853d65eb9b7535f3077ebf525a82d5bf132fb
Tropical <up201306506@gcloud.fe.up.pt>
2016-11-22 15:42:17 +0000

    DB 50%

--- Code/controllers/MessageController.js
+++ Code/controllers/MessageController.js
@@ -1,25 +1,60 @@
+var assert = require('assert');
 var configDB = require('./../config/dbURL.js');
 
 module.exports = {
-    
-    insertMessage : function (db, id, sender, receiver, content, date, type, callback ) {
+
+    //Create a new Message
+    insertMessage : function (db, sender, receiver, content, date, type, callback) {
+        // Get messages collection
         var messages = db.collection('messages');
-        messages.find({_id:id}).toArray(function (err,docs) {
-            assert.equal(err,null);
-            if(docs.length>=1){
-                console.log('Already exists a message with the giver id');
-                db.close();
-            }
-            else{
-                messages.insertOne({_id:id, sender:sender, receiver:receiver, content:content, date:date, type:type},
-                    function (err,result) {
-                        assert.equal(err,null);
-                        assert.equal(1, result.result.n);
-                        assert.equal(1, result.ops.length);
-                        console.log('Inserted 1 document into the db');
-                        callback(result);
-                    });
-            }
+
+        //add message to collection
+        messages.insertOne({sender:sender, receiver:receiver, starred:false, content:content, date:date, type:type, deleted:false},
+            function (err,result) {
+                assert.equal(err,null);
+                assert.equal(1, result.result.n);
+                assert.equal(1, result.ops.length);
+                console.log('Inserted 1 document into the db');
+                callback(result);
+            });
+    },
+
+
+    //Get specific message with id
+    getMessageByID : function (db, id, callback ) {
+        var messages = db.collection('messages');
+
+        messages.find( { _id:id } ).toArray(function(err,result){
+            assert.equal(err, null);
+            callback(result);
+        });
+    },
+
+
+    //Get messages sent to user
+    getUserMessages : function(db, username, callback) {
+
+        var messages = db.collection('messages');
+
+        messages.find( { receiver:username} ).toArray(function(err,result){
+            assert.equal(err, null);
+            callback(result);
+        });
+
+    },
+
+
+    //Get messages to user with star
+    //Get messages of a specific type
+    //Get messages with the flag "deleted"
+
+    //Remove Message
+    removeMessage : function ( db, id, callback) {
+        var messages = db.collection('messages');
+        messages.deleteOne({_id: id}, function (err, result) {
+            assert.equal(err, null);
+            callback(result);
         });
     }
+
 }
