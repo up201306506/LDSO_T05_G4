7a8d471452710afa5686fa75f2dceef50966bb4d
telmo08 <telmo_8b@hotmail.com>
2016-12-02 21:19:04 +0000

    Pagination - Community page

--- Code/routes/community/community_page.js
+++ Code/routes/community/community_page.js
@@ -12,10 +12,21 @@ router.get('/:communityName', userPrivileges.ensureAuthenticated, function (req,
 
     // Connects to the db
     mongo.connect(configDB.url, function (err, db) {
+
+        //Verifica em que página está
+        var page = req.query.page;
+        if(!page){
+            page = 1;
+        }
+
+        //Range
+        var pageSize = 10;
+        var range = {from:(pageSize*(page-1)), size:pageSize};
+
         // Verifies if this user is enrolled in this community
         communityController.isUserEnrolledInCommunity(db, req.user, communityName, function (community) {
             // Get this community offers
-            offerController.getCommunityOffers(db, communityName, function (offers) {
+            offerController.getCommunityOffers(db, communityName, range, function (offers, totalOffersCount) {
                 db.close();
 
                 // If user is not enrolled in the community, no community should have been returned
@@ -25,7 +36,9 @@ router.get('/:communityName', userPrivileges.ensureAuthenticated, function (req,
                     res.render('community/community_page',
                         {
                             communityName: communityName,
-                            offerArr: offers
+                            offerArr: offers,
+                            nPages: Math.ceil(totalOffersCount/2),
+                            thisPage: page
                         });
                 }
             });
