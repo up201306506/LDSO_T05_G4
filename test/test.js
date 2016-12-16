fe51a07a9a26128ec2a2a5cea853ed75251ca904
Tropical <up201306506@gcloud.fe.up.pt>
2016-12-16 17:18:08 +0000

    e deixame remover este console.log

--- test/test.js
+++ test/test.js
@@ -1,8 +1,5 @@
 var assert = require('assert');
 var configDB = require('./../config/dbURL.js');
-console.log(configDB.url);
-
-
 assert.equal('mongodb://LDSO:ldso123456@ds155747.mlab.com:55747/heroku_gdqp38z3', configDB.url);
 
 
