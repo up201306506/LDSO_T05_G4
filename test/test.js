37419d89dcbe64b0ef1fd474d30ca9430535181c
Tropical <up201306506@gcloud.fe.up.pt>
2016-12-16 17:15:44 +0000

    Isto DEVE funcionar

--- test/test.js
+++ test/test.js
@@ -3,7 +3,7 @@ var configDB = require('./../config/dbURL.js');
 console.log(configDB.url);
 
 
-assert.equal('mongodb://LDSO:ldso123456@ds155747.mlab.com:55747/heroku_gdqp38z', configDB.url);
+assert.equal('mongodb://LDSO:ldso123456@ds155747.mlab.com:55747/heroku_gdqp38z3', configDB.url);
 
 
 
