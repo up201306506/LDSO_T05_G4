9887a07d0193e41a26371db75c043a4315d626b9
Tropical <up201306506@gcloud.fe.up.pt>
2016-12-16 21:24:05 +0000

    Travis properly checks for local database connection

--- test/test.js
+++ test/test.js
@@ -3,66 +3,92 @@ var configDB = require('./../config/dbURL.js');
 var mongo = require('mongodb').MongoClient;
 var messagingController = require("./../controllers/MessageController.js");
 
-//Guarantee that no one sent the wrong db link.
-assert.equal('mongodb://LDSO:ldso123456@ds155747.mlab.com:55747/heroku_gdqp38z3', configDB.url);
+describe("DB URL variable", function() {
+	it("seriously, don't commit local variables over the remote one", function() {
+		//Guarantee that no one sent the wrong db link.
+		assert.equal('mongodb://LDSO:ldso123456@ds155747.mlab.com:55747/heroku_gdqp38z3', configDB.url);
+	});
+});
 
-//Assuming travis, mongod should be running locally
-configDB.url = 'mongodb://localhost:27017/LocalExchangeDB';
-
-mongo.connect(configDB.url, function (err, db) {
+describe('Database', function() {
+	DBurl = 'mongodb://localhost:27017/test';
 	
-	//-------------
-	//	TEST
-	//-------------
-	/*
-		Insert a generic message, it should exist in the db
-		Coverage: 
-			MessageController.insertMessage() - Success
-			MessageController.getMessagesByUser() - Success
-			MessageController.getSentByUser() - Success
-	*/
-	describe('MessageController', function() {
-		describe('Inserting and checking a message was inserted', function() {
-			it('should work...', function() {
-				messagingController.insertMessage(db, "testuser", "testreceiver", "testsubject", "testcontent", new Date(), "offer", function(success){});
-				messagingController.getMessagesByUser(db, "testreceiver", function(userMessages){
-					assert.equal(userMessages.length, 1);
-				});
-				messagingController.getSentByUser(db, "testuser", function(userMessages){
-					assert.equal(userMessages.length, 1);
-				});
-				messagingController.insertMessage(db, "testuser", "testreceiver", "testsubject", "testcontent", new Date(), "offer", function(success){});
-				messagingController.insertMessage(db, "testuser", "testreceiver", "testsubject", "testcontent", new Date(), "offer", function(success){});
-				messagingController.insertMessage(db, "testreceiver", "testuser", "testsubject", "testcontent", new Date(), "offer", function(success){});
-				messagingController.getMessagesByUser(db, "testreceiver", function(userMessages){
-					assert.equal(userMessages.length, 3);
-				});
-				messagingController.getSentByUser(db, "testuser", function(userMessages){
-					assert.equal(userMessages.length, 3);
-				});
-				messagingController.getMessagesByUser(db, "testuser", function(userMessages){
-					assert.equal(userMessages.length, 1);
-				});
-				messagingController.getSentByUser(db, "testreceiver", function(userMessages){
-					assert.equal(userMessages.length, 1);
-				});
-
-			});
+	
+    // runs before all tests in this block	
+	before('there must be a local test database and it must be reachable', function(done) {
+		mongo.connect(DBurl, function (err, db) {
+			if (err) done(err);
+			db.close();
+			done();
 		});
-		
-		
+	});
+
+	
+	
+	it("It doesn't matter whay I put here", function() {			
 	});
 	
-	db.close();
+	
+	
+	/*
+	describe('Messaging', function() {
+		it('Inserting and checking a message was inserted', function(done) {
+			
+			done();
+		});
+	});
+	*/
 });
-	
-	
+
+
+/*	
+if(err){
+	console.log("welp");
+} else {
+	console.log("YYYYY");
+}
+
+
+//-------------
+//	TEST
+//-------------
 /*
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal(-1, [1,2,3].indexOf(4));
-    });
-  });
+	Insert a generic message, it should exist in the db
+	Coverage: 
+		MessageController.insertMessage() - Success
+		MessageController.getMessagesByUser() - Success
+		MessageController.getSentByUser() - Success
+*//*
+describe('MessageController', function() {		
+	describe('Inserting and checking a message was inserted', function() {
+		it('should work', function() {
+			messagingController.insertMessage(db, "testuser", "testreceiver", "testsubject", "testcontent", new Date(), "offer", function(success){});
+			messagingController.getMessagesByUser(db, "testreceiver", function(userMessages){
+				assert.equal(userMessages.length, 2);
+			});
+			messagingController.getSentByUser(db, "testuser", function(userMessages){
+				assert.equal(userMessages.length, 1);
+			});
+			messagingController.insertMessage(db, "testuser", "testreceiver", "testsubject", "testcontent", new Date(), "offer", function(success){});
+			messagingController.insertMessage(db, "testuser", "testreceiver", "testsubject", "testcontent", new Date(), "offer", function(success){});
+			messagingController.insertMessage(db, "testreceiver", "testuser", "testsubject", "testcontent", new Date(), "offer", function(success){});
+			messagingController.getMessagesByUser(db, "testreceiver", function(userMessages){
+				assert.equal(userMessages.length, 3);
+			});
+			messagingController.getSentByUser(db, "testuser", function(userMessages){
+				assert.equal(userMessages.length, 3);
+			});
+			messagingController.getMessagesByUser(db, "testuser", function(userMessages){
+				assert.equal(userMessages.length, 1);
+			});
+			messagingController.getSentByUser(db, "testreceiver", function(userMessages){
+				assert.equal(userMessages.length, 1);
+			});
+
+		});
+	});
+	
+	
 });
+
 */
