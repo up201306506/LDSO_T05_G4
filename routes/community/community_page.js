55d37bee806c4faf3545503c57a67c9710e3cfa8
Tropical <up201306506@gcloud.fe.up.pt>
2016-12-27 20:27:47 +0000

    Butão de entrar na comunidade fixed

--- routes/community/community_page.js
+++ routes/community/community_page.js
@@ -27,50 +27,43 @@ router.get('/:communityName', userPrivileges.ensureAuthenticated, function (req,
             // Get this community offers
             offerController.getCommunityOffers(db, communityName, range, function (offers, totalOffersCount) {
                 // Closes db
-                db.close();
 
                 var isModeratorBool = false;
-
-                console.log(req.user);
-                console.log(isModeratorBool);
-
-                console.log(community.admins);
 
                 if(community.admins.indexOf(req.user) != -1)
                     isModeratorBool = true;
 
-                console.log(isModeratorBool);
-
-                // Renders page
-                res.render('community/community_page',
-                    {
-                        communityName: community.name,
-                        communityOffice: community.office,
-                        communityDescription: community.description,
-                        communityRules: community.ruleDescription,
-                        useCoin: community.useCoin,
-                        coinName: community.coinName,
-                        privacy: community.privacy,
-                        admins: community.admins,
-                        members: community.members,
-                        offerArr: offers,
-                        isMod: isModeratorBool,
-                        nPages: Math.ceil(totalOffersCount/2),
-                        thisPage: page,
-                        user: req.user
-                    });
+                communityController.isUserEnrolledInCommunity(db, req.user, communityName, function (isMemberBool) {
+                    db.close();
+                    // Renders page
+                    res.render('community/community_page',
+                        {
+                            communityName: community.name,
+                            communityOffice: community.office,
+                            communityDescription: community.description,
+                            communityRules: community.ruleDescription,
+                            useCoin: community.useCoin,
+                            coinName: community.coinName,
+                            privacy: community.privacy,
+                            admins: community.admins,
+                            members: community.members,
+                            offerArr: offers,
+                            isMod: isModeratorBool,
+                            isMember: isMemberBool,
+                            nPages: Math.ceil(totalOffersCount/2),
+                            thisPage: page,
+                            user: req.user
+                        });
+                });
             });
         });
     });
 });
 
-
 router.get('/:communityName/join_community/', userPrivileges.ensureAuthenticated, function (req, res, next) {
 // GET community name from url
     var communityName = String(req.params.communityName);
     mongo.connect(configDB.url, function (err, db) {
-
-
         communityController.isUserEnrolledInCommunity(db, req.user, communityName, function (community) {
             communityController.getCommunityData(db, communityName, function (result) {
 
@@ -78,18 +71,24 @@ router.get('/:communityName/join_community/', userPrivileges.ensureAuthenticated
                 if (community == false) {
                     if(result.privacy == "Privada") {
                         communityController.insertRequests(db, communityName, req.user, function (callb) {
+                            req.flash('success_msg', 'Pedido de entrada na comunidade: '+ communityName + ' enviado aos administradores');
+                            console.log("User: " + req.user + "requested to join community: " + communityName);
                             db.close();
+                            res.redirect("/community/"+communityName);
                         });
                     } else if(result.privacy == "Pública") {
                         communityController.insertUserInCommunity(db, communityName, req.user, function (result) {
+                            req.flash('success_msg', 'Entraste na comunidade: ' + communityName);
+                            console.log("User: " + req.user + "joined community: " + communityName);
                             db.close();
+                            res.redirect("/community/"+communityName);
                         });
                     }
                 // Else meh
                 } else {
-
-                    //....
-
+                    req.flash('error_msg', 'Já pertences à comunidade');
+                    db.close();
+                    res.redirect("/community/"+communityName);
                 }
 
             });
@@ -97,8 +96,6 @@ router.get('/:communityName/join_community/', userPrivileges.ensureAuthenticated
         });
 
     });
-    res.redirect("/community/"+communityName);
-
 });
 
 module.exports = router;
