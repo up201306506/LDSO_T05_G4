3468267de517c1bb8f72d791e45815639b7af8df
JoaoIsaias <jl.isaias123@gmail.com>
2016-12-27 19:29:00 +0000

    Porque é que não dá? :C

--- config/dbURL.js
+++ config/dbURL.js
@@ -1,4 +1,4 @@
 module.exports = {
-    //'url' : 'mongodb://localhost:27017/LocalExchangeDB', // mongodb://<user>:<pass>@localhost:27017/schema
-    'url' : 'mongodb://LDSO:ldso123456@ds155747.mlab.com:55747/heroku_gdqp38z3'
+    'url' : 'mongodb://localhost:27017/LocalExchangeDB'//, // mongodb://<user>:<pass>@localhost:27017/schema
+    //'url' : 'mongodb://LDSO:ldso123456@ds155747.mlab.com:55747/heroku_gdqp38z3'
 };
