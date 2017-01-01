82233572ef90d6a303e6bc382ec0a484595651fa
PedroJoaoPereira <pedro.pereira.95@hotmail.com>
2017-01-01 19:00:15 +0000

    Small changes

--- app.js
+++ app.js
@@ -83,7 +83,6 @@ var webpage_createCommunity = require('./routes/community/create_community');
 var webpage_editCommunity = require('./routes/community/edit_community');
 var webpage_communityUserList = require('./routes/community/community_user_list');
 var webpage_accept_requests = require('./routes/community/accept_requests');
-var webpage_deleteCommunity = require('./routes/community/remove_community');
 // offer
 var webpage_createOffer = require('./routes/offer/create_offer');
 var webpage_editoffer = require('./routes/offer/edit_offer');
@@ -106,7 +105,6 @@ app.use('/create_community', webpage_createCommunity);
 app.use('/edit_community', webpage_editCommunity);
 app.use('/community_users', webpage_communityUserList);
 app.use('/accept_requests', webpage_accept_requests);
-app.use('/delete_community', webpage_deleteCommunity);
 // offer
 app.use('/create_offer', webpage_createOffer);
 app.use('/edit_offer', webpage_editoffer);
